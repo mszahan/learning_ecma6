11838e58466926123013d5fe460e9ab93e2ac800
mszahan <mszahan1@gmail.com>
2022-10-16 21:08:35 +0600

    fetch github api though it didn't work

--- async_await.js
+++ async_await.js
@@ -16,3 +16,18 @@ const countToFive = async () => {
 };
 
 countToFive();
+
+
+
+
+// another example
+const githubRequest = async (login) => {
+    let response = await fetch(
+        `https://api.github.com/users/${login}`
+    );
+    let json = await response.json();
+    let summary = `${json.name}, ${json.company}`;
+    console.log(summary);
+};
+
+githubRequest('mszahan');
