8721c5aad4d7e9dc9719ed83e6645cd933b56ba1
mszahan <mszahan1@gmail.com>
2022-10-16 20:35:42 +0600

    use promises to fetch api data of spacePeople

--- check_promises.js
+++ check_promises.js
@@ -20,3 +20,30 @@ delay(3).then(() => console.log('three second'));
 
 // this error is comming before any delay code is running
 // delay('hola').then(() => console.log('three second'));
+
+
+// working with api using promises
+const spacePeople = () => {
+    return new Promise((resolves, rejects) => {
+        const api = 'http://api.open-notify.org/astros.json';
+        const request = new XMLHttpRequest();
+        request.open('GET', api);
+        request.onload = () => {
+            if (request.status === 200) {
+                resolves(JSON.parse(request.response));
+            
+            }
+            else {
+                rejects(Error(request.statusText));
+
+            }
+        };
+        request.onerror = (err) => rejects(err);
+        request.send();
+    });
+};
+
+spacePeople().then((spaceData) => 
+console.log(spaceData)
+// (err) => console.error(new Error('cant load the people'))
+);
